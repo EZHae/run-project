33fcac1bdd0e0c0420fb0a05490d64cb9ec04948
Jihae Lee <wlgo0068@naver.com>
2025-01-21 18:40:16 +0900

    courseDao TODO

--- running/src/main/java/com/itwill/running/repository/CourseDao.java
+++ running/src/main/java/com/itwill/running/repository/CourseDao.java
@@ -3,6 +3,7 @@ package com.itwill.running.repository;
 import java.util.List;
 
 import com.itwill.running.domain.Course;
+import com.itwill.running.domain.User;
 
 public interface CourseDao {
 	
@@ -17,4 +18,9 @@ public interface CourseDao {
 	int updateCourse(Course course);
 	
 	int deleteCourse(Integer id);
+	
+	// TODO
+	List<User> selectLikeUserId(Integer courseId); // 코스 아이디를 검색하여 해당 코스에 좋아요한 유저의 리스트 리턴
+	
+	int updateLikeCount(Integer id); // 조건을 만족했을 때 코스의 아이디로 검색하여 해당 코스의 like_count +1
 }
