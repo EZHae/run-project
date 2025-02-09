2c51f11739d62f5b5348e71ab9faf2d84b4a7b1f
yujinHeo <youjin2586@gmail.com>
2025-02-09 20:02:03 +0900

    last commit TCalendar

--- running/src/main/java/com/itwill/running/service/UserService.java
+++ running/src/main/java/com/itwill/running/service/UserService.java
@@ -3,7 +3,6 @@ package com.itwill.running.service;
 import org.springframework.stereotype.Service;
 
 import com.itwill.running.domain.User;
-import com.itwill.running.dto.UserItemDto;
 import com.itwill.running.repository.UserDao;
 
 import lombok.RequiredArgsConstructor;
@@ -16,13 +15,10 @@ public class UserService {
 	
 	private final UserDao userDao;
 	
-	public UserItemDto selectByUserId(String userId) {
+	public User selectByUserId(String userId) {
 		log.debug("selectByUserId(UserId={})", userId);
 		User user = userDao.selectByUserId(userId);
-		if (user != null) {
-            return UserItemDto.fromEntity(user);
-        } else {
-            return null;
-        }
+		
+		return user;
 	}
 }
